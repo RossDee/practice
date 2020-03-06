844d10ebc31f45be5c8cdd720ca68913ee84dd8f
Ross <diying@sjtu.edu.cn>
2020-03-06 09:33:22 +0800

    tree update

--- trees.py
+++ trees.py
@@ -6,9 +6,6 @@ featVec
 currentLabel
 prob:key在实例中的概率
 log(prob,2)
-
-
-
 '''
 from math import log
 def CalShannonEnt(dataset):
@@ -40,9 +37,7 @@ values,需要返回的特征的值
 return 
 retDataSet:将特征列排除后的数据集，即将axis 对应的行数据排除
 featVec 
-
 '''
-
 def splitDataSet(dataset,axis,values):
     retDataSet = []
     for featVec in dataset:
@@ -56,5 +51,31 @@ def splitDataSet(dataset,axis,values):
 '''
 def chooseBestFeatureToSplit
 input dataSet
-
+numFeatures:特征的数量
+baseEntropy: 对dataset计算CalShannonEnt ，计算整个数据集的原始ShannoonEntropy
+bestInfoGain:最好的信息增益
+bestFeature 
+featList
+uniqueVals
+Step1:获取列表中唯一元素值uniqueVals，通过遍历数据集中的所有特征，然后通过列表推导来创建新的列表featList
+Step2:对每种划分方式计算熵 subDataSet,prob,newEntropy,
+step3:计算最好的信息增益,即熵减
 '''
+def chooseBestFeatureToSplit(dataset):
+    numFeatures = len(dataset[0])-1
+    baseEntropy = CalShannonEnt(dataset)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataset]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataset,i,value)
+            prob = len(subDataSet)/float(len(dataset))
+            newEntropy += prob * CalShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain >baseEntropy):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
